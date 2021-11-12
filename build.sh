31aeff81918db68cffc55c9d57cf5d4f4faa7740
Alexander Popov <iiiypuk@iiiypuk.me>
2021-11-12 07:44:58 +0300

    change output filename

--- build.sh
+++ build.sh
@@ -5,4 +5,11 @@ then
     mkdir bin
 fi
 
-crystal build --progress --no-debug --release src/vk0nline.cr -o bin/vk0nline.x86_64-lnx
+ARCH=`uname -m`
+OS=`uname | tr '[:upper:]' '[:lower:]'`
+VERSION=`cat shard.yml | grep -m 1 'version: ' | sed -e 's/version: //g'`
+
+echo "Build $VERSION for `uname` `uname -m`"
+
+crystal build --progress --no-debug --release src/vk0nline.cr -o bin/vk0nline-$VERSION.$OS-$ARCH
+
